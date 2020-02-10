7460315f11fa697d5c5e3d4c2d40bb41e623e056
Muhammad Gifar Zaini <gifarz06@gmail.com>
2020-02-10 11:20:03 +0700

    gifar

--- src/Components/Pages/LandingPage.js
+++ src/Components/Pages/LandingPage.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Landing = () => {
     return (
+        <>
         <h1>Landing Page Aqu Donk</h1>
+        <h1>Landing Page Aqu2 Donk</h1>
+        </>
     )
 }
 
